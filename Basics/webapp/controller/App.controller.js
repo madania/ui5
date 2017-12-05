1562953a3128d4868513d8bae07fe844ba41797c
Harshit Madania <harshitmadania@MacBook-ProB.local>
2017-12-05 14:01:52 +0300

    i18n

--- Basics/webapp/controller/App.controller.js
+++ Basics/webapp/controller/App.controller.js
@@ -1,21 +1,32 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel"
- ], function (Controller,MessageToast,JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
     "use strict";
     return Controller.extend("sap.ui.demo.wt.controller.App", {
-        onShowHello: function(){
-            MessageToast.show("Hello World");
-        },
-        onInit:function(){
-            var oData={
-                    recipient:{
-                        name:"World"
-                    }
+
+        onInit: function () {
+            var oData = {
+                recipient: {
+                    name: "World"
+                }
             }
-            var oModel=new JSONModel(oData);
+            var oModel = new JSONModel(oData);
             this.getView().setModel(oModel);
-        }
+
+            var i18nModel = new ResourceModel({
+                bundleName: "sap.ui.demo.wt.i18n.i18n"
+            });
+            this.getView().setModel(i18nModel, "i18n");
+
+        },
+        onShowHello: function () {
+            var oBundle=this.getView().getModel("i18n").getResourceBundle();
+            var sRecipient= this.getView().getModel().getProperty("/recipient/name");
+            var sMsg=oBundle.getText("helloMsg",[sRecipient]);
+            MessageToast.show(sMsg);
+        },
     });
- });
+});
