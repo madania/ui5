3a1cc0488b59cd53561cc4fe6922c32915584a91
Harshit Madania <harshitmadania@MacBook-ProB.local>
2017-12-06 13:38:37 +0300

    Dialogs

--- Basics/webapp/controller/HelloPanel.controller.js
+++ Basics/webapp/controller/HelloPanel.controller.js
@@ -14,6 +14,15 @@ sap.ui.define([
         
             // show message
             MessageToast.show(sMsg);
+        },
+        onOpenDialog:function(){
+            var oView=this.getView();
+            var oDialog=oView.byId("helloDialog");
+            if(!oDialog){
+                oDialog=sap.ui.xmlfragment(oView.getId(), "sap.ui.demo.wt.view.HelloDialog");
+                oView.addDependent(oDialog);
+            }
+            oDialog.open();
         }
     });
 
